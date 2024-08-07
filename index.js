4a828ebc39d5e50790fd63b7b23c5581ea1d0115
Yogesh Choudhary <127961738+Yogesh-ok55@users.noreply.github.com>
2024-08-08 02:29:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 
 app.get("/",(req,res)=>{
-    res.send("<h1>Hello yogesh all the best for vercel practise</h1>")
+    res.send("<h1>Hello yogesh all the best for vercel practise can you see these changes in website</h1>")
 })
 
 app.listen(3000, ()=>{
